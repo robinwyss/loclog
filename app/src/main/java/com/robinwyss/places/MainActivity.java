44d35e24a40470f57840612751b5b4a88cc8d324
Robin Wyss <robin@configit.com>
2017-08-18 13:09:10 -0400

    added text view for last know location

--- app/src/main/java/com/robinwyss/places/MainActivity.java
+++ app/src/main/java/com/robinwyss/places/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
@@ -35,15 +36,22 @@ public class MainActivity extends AppCompatActivity {
     private LocationCallback mLocationCallback;
     private static final String TAG = "MainActivity";
     private TextView textView;
+    private DateFormat dateFormat;
+    private DateFormat timeFormat;
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Location location = (Location)intent.getParcelableExtra(Constants.LOCATION_UPDATE_DATA);
-            DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
-            DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(getApplicationContext());
+            final Location location = intent.getParcelableExtra(Constants.LOCATION_UPDATE_DATA);
 
-            textView.setText(textView.getText() + String.format(" %s %s - lat: %f, long: %f, %f %n",timeFormat.format(location.getTime()), dateFormat.format(location.getTime()), location.getLatitude(), location.getLongitude(), location.getAccuracy()));
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    UpdateLastKnownLocation(location);
+                    textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(textView.getText() + String.format("%s %s - lat: %f, long: %f, %f %n", timeFormat.format(location.getTime()), dateFormat.format(location.getTime()), location.getLatitude(), location.getLongitude(), location.getAccuracy()));
+                }
+            });
         }
     };
 
@@ -52,22 +60,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       textView = (TextView) findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
+        dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        timeFormat = android.text.format.DateFormat.getTimeFormat(getApplicationContext());
 
         setSupportActionBar(toolbar);
         Intent mServiceIntent = new Intent(this, LocationService.class);
         startService(mServiceIntent);
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(receiver,  new IntentFilter(
+//        LocationReceiver receiver = new LocationReceiver(new Handler());
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(
                 Constants.LOCATION_UPDATE));
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         getLocation();
     }
 
-    private void printLocations(ArrayList<String> locations) {
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(TextUtils.join("\n", locations));
+    private void UpdateLastKnownLocation(Location location) {
+        final TextView textView = (TextView) findViewById(R.id.currentLocation);
+        textView.setText(String.format("%s: lat: %f, long: %f, ", timeFormat.format(location.getTime()), location.getLatitude(), location.getLongitude()));
     }
 
     private void GetLastKnowLocation() {
@@ -81,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-                            textView.setText(String.format("lat: %f, long: %f, altitude, %f", location.getLatitude(), location.getLongitude(), location.getAltitude()));
+                            UpdateLastKnownLocation(location);
                         } else {
                             Toast.makeText(getApplicationContext(), "Could not get last location", Toast.LENGTH_SHORT).show();
                         }
@@ -98,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
     private void getLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-            Toast.makeText(getApplicationContext(), "Need location permission",  Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Need location permission", Toast.LENGTH_SHORT).show();
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
         } else {
             GetLastKnowLocation();
